6b911466a14e69b0510ffd6f5189c454919dffc3
Senior Expert of Web & Mobile development <funnyventure@outlook.com>
2018-09-18 23:09:42 -0700

    fix notigcation

--- src/components/Dashboard/Views/Notifications.vue
+++ src/components/Dashboard/Views/Notifications.vue
@@ -20,11 +20,11 @@
             <span data-notify="icon" class="ti-bell"></span>
             <span data-notify="message">This is a notification with close button and icon.</span>
           </div>
-          <div class="alert alert-info alert-with-icon" data-notify="container">
+          <!-- <div class="alert alert-info alert-with-icon" data-notify="container">
             <button type="button" aria-hidden="true" class="close">×</button>
             <span data-notify="icon" class="ti-pie-chart"></span>
             <span data-notify="message">This is a notification with close button and icon and have many lines. You can see that the icon and the close button are always vertically aligned. This is a beautiful notification. So you don't have to worry about the style.</span>
-          </div>
+          </div> -->
         </div>
         <div class="col-md-6">
           <h5>Notification states</h5>
